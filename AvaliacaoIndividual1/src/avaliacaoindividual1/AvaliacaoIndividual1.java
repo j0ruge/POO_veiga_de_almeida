e57b1f6bd76b3005785d5a1f715d0e2cd27a3e8b
JorUge <joruge@gmail.com>
2021-05-05 17:00:04 -0300

    Avaliação Individual

--- AvaliacaoIndividual1/src/avaliacaoindividual1/AvaliacaoIndividual1.java
+++ AvaliacaoIndividual1/src/avaliacaoindividual1/AvaliacaoIndividual1.java
@@ -24,6 +24,7 @@ public class AvaliacaoIndividual1 {
         Lancha l3 = new Lancha("Intermarine", "380 Full", "branca", "9V6191", "fibra de vidro"); //ref. Lancha Intermarine 380 Full
         Lancha l4 = new Lancha(4, 180000.00, 23, 0.5, 320); //ref. Lancha Triton 280 
         Lancha l5 = new Lancha("branca", 5, 34, 0.9, 520, "fibra de vidro"); // ref. Lancha Portofino Fly 42
+        Lancha l6 = new Lancha(); 
 
         
         // Jetskis
@@ -33,6 +34,7 @@ public class AvaliacaoIndividual1 {
         Jetski j3 = new Jetski(3, 79, 0.1, "GTX215", 1494, true); //ref. Seadoo GTX 215
         Jetski j4 = new Jetski("Yamaha", "VX 1100", "vermelho", "Leone"); // ref. Jet Ski VX 1100
         Jetski j5 = new Jetski("azul/branco", 3, 65, 0.2, 0, "fibra", true);
+        Jetski j6 = new Jetski(); 
         
         // Iates
         
@@ -41,12 +43,14 @@ public class AvaliacaoIndividual1 {
         Iate i3 = new Iate("INACE", "82 YACHT", "branca", "POQTY"); // INACE 82 YACHT
         Iate i4 = new Iate(5, 13, 1.5, "Fernando de Almeida", 3, 2000); // ref. INACE EXPLORA 90 
         Iate i5 = new Iate(10, 3000000.00, 13, 2, 3, 2900, 5); //INACE EXPLORA 115
-        Iate i6 = new Iate();
+        Iate i6 = new Iate(); // FM/Y Azzam
         
+        l6.cadastrar("FS Yachts", "FS 180", "branca", 7,180000.00 , 39, 0.4, "FS180", 130, "fibra", true); // ref. FS 180 
         
+        j6.cadastrar("Yamaha", "FX Cruiser HO", "Azure Blue with White", 3, 65000.00, 52, 0.1, "FXCRUISER", 180, "fibra", true); // ref. FX Cruiser HO
         
         i6.cadastrar("Lurssen", "Custom", "branca", 36, 600000000.00, 34, 4.5, "M/Y Azzam", 20, 13136, 18, true); // re. M/Y Azzam
-        
+     
         
         l1.imprimir();
         l2.imprimir();
@@ -70,13 +74,35 @@ public class AvaliacaoIndividual1 {
         i5.imprimir();
         System.out.println("Sugestão de " + i5.pessoasPorCabine() + " pessoas por cabine");
         
+        
+        
+        j6.imprimir();
+        System.out.println("");
+        System.out.println("::::INFORMAÇÕES ADICIONAIS::::");             
+        System.out.println("Valor máximo de desconto permitido: R$ " +                
+        String.format(Locale.GERMAN,"%,.2f", j6.valorDesconto() )
+        );
+        
+        
+        
+        
+        l6.imprimir();
+        
+         System.out.println("");
+        System.out.println("::::INFORMAÇÕES ADICIONAIS::::");             
+        System.out.println("Valor máximo de desconto permitido: R$ " +                
+                String.format(Locale.GERMAN,"%,.2f", l6.valorDesconto() )
+        );
+        
+        
+        
         i6.imprimir();
         
         System.out.println("");
         System.out.println("::::INFORMAÇÕES ADICIONAIS::::");
         System.out.println("Sugestão de " + i6.pessoasPorCabine() + " pessoas por cabine.");
         
-        System.out.println("Valor máximo de desconto permitido: " +                
+        System.out.println("Valor máximo de desconto permitido: R$ " +                
                 String.format(Locale.GERMAN,"%,.2f", i6.valorDesconto() )
         );
                 
