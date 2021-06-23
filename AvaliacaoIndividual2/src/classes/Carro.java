7ac846ed64640a409e6e40f3d6595dc6007e4fcc
j0ruge <46424602+j0ruge@users.noreply.github.com>
2021-06-22 21:57:30 -0300

    Adição da Classe Carro

---
+++ AvaliacaoIndividual2/src/classes/Carro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author JorUge
+ */
+public class Carro extends MeioTransporte{
+
+    @Override
+    protected double valorDesconto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
---
+++ AvaliacaoIndividual2/src/classes/MeioTransporte.java
@@ -0,0 +1,173 @@
+package classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author JorUge
+ */
+public abstract class MeioTransporte {
+    protected String marca;
+    protected String modelo;
+    protected String cor;
+    protected double comprimento;
+    protected double largura;
+    protected double preco;
+    
+     // Construtores
+    
+    
+    
+     // 1/8 - Construtor vazio.
+    protected MeioTransporte(){};
+    
+        
+    // 2/8 - Atributos do tipo Texto
+    protected MeioTransporte(String marca, String modelo, String cor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;       
+    
+    }       
+    
+    // 3/8 - Atributos do tipo real
+    protected MeioTransporte(double comprimento, double largura, double preco){
+        this.comprimento = comprimento;
+        this.largura = largura;
+        this.preco = preco;
+    };
+    
+    // 4/8 - Todos atributos
+    protected MeioTransporte(
+            String marca,
+            String modelo,
+            String cor,
+            double comprimento,
+            double largura,
+            double preco ){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.comprimento = comprimento;
+        this.largura = largura;
+        this.preco = preco;
+        
+    };
+    
+    
+    // 5/8 - Atributos Marca e Modelo 
+    protected MeioTransporte(String marca, String modelo ){
+        this.marca = marca;
+        this.modelo = modelo;   
+    };
+    
+    
+    // 6/8 - Atributo estético 
+    protected MeioTransporte(String cor ) {
+        this.cor = cor;        
+    }
+    
+    // 7/8 - Atributos de dimensão
+    protected MeioTransporte(
+                    double comprimento,
+                    double largura                    
+    ) {
+        this.comprimento = comprimento;
+        this.largura = largura;
+        
+    }
+    
+
+    // 8/8 - Atributos operacionais
+    protected MeioTransporte(double preco) {
+        this.preco = preco;        
+        
+    };   
+    
+        
+    // Métodos de Acesso  
+
+    protected String getMarca() {
+        return marca;
+    }
+
+    protected void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    protected String getModelo() {
+        return modelo;
+    }
+
+    protected void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    protected String getCor() {
+        return cor;
+    }
+
+    protected void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    protected double getComprimento() {
+        return comprimento;
+    }
+
+    protected void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    protected double getLargura() {
+        return largura;
+    }
+
+    protected void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    protected double getPreco() {
+        return preco;
+    }
+
+    protected void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    // Métodos extras
+    
+    protected abstract double valorDesconto();
+    
+    protected void entrada() {
+        System.out.println("");
+        System.out.println("INSIRA OS DADOS SOLICITADOS:");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Marca             : ");
+        setMarca(input.nextLine());    
+        
+        System.out.print("Modelo            : ");
+        setModelo(input.nextLine());
+        
+        System.out.print("Cor               : ");
+        setCor(input.nextLine());    
+        
+        System.out.print("Comprimento             : ");
+        setPreco(Double.parseDouble(input.nextLine()));
+        
+        System.out.print("Largura             : ");
+        setPreco(Double.parseDouble(input.nextLine()));
+        
+        System.out.print("Preço             : ");
+        setPreco(Double.parseDouble(input.nextLine()));               
+    
+    }
+    
+    
+}
