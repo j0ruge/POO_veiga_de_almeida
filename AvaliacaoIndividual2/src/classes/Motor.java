01777833c3cc50371a7c4b598b108b63a84df451
j0ruge <46424602+j0ruge@users.noreply.github.com>
2021-06-24 12:03:21 -0300

    Organização de código

--- AvaliacaoIndividual2/src/classes/Motor.java
+++ AvaliacaoIndividual2/src/classes/Motor.java
@@ -5,7 +5,6 @@
  */
 package classes;
 
-
 import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
@@ -15,147 +14,167 @@ import java.util.Scanner;
  * @author jorge.ferrari
  */
 public class Motor {
-    
-    
+
     private double peso;
     private int rpm;
-    private int velocidade;    
-    private String tipo;    
+    private int velocidade;
+    private String tipo;
     private double preco;
-    
-    
-    // Construtores
+
+    // <editor-fold defaultstate="collapsed" desc=" Construtores ">
     // 1/5 - Construtor vazio.
     public Motor() {
-    };
+    }
+
+    ;
     
         
     // 2/5 - Atributos do tipo Texto
     public Motor(String tipo) {
-        this.tipo = tipo;        
+        this.tipo = tipo;
 
-    };
+    }
+
+    ;
 
     // 3/5 - Atributos do tipo real
     public Motor(double peso, double preco) {
         this.peso = peso;
         this.preco = preco;
-    };
+    }
+
+    ;
     
     // 4/5 - Todos atributos
     public Motor(
             double peso,
             int rpm,
-            int velocidade,    
-            String tipo,    
+            int velocidade,
+            String tipo,
             double preco) {
         this.peso = peso;
         this.rpm = rpm;
         this.velocidade = velocidade;
         this.tipo = tipo;
         this.preco = preco;
-    };
+    }
+
+    ;
     
     
     // 5/5 - Atributos comerciais
     public Motor(double preco) {
         this.preco = preco;
-    };    
+    }
+
+    ;    
     
+
+    // </editor-fold>
+
     
-    // Métodos de Acesso 
+    // <editor-fold defaultstate="collapsed" desc=" Métodos de Acesso ">
+      
     
-    public double getPeso() {
+    // <editor-fold defaultstate="collapsed" desc=" Getters ">
+
+public double getPeso() {
         return peso;
     }
 
     public void setPeso(double peso) {
-        
-        
-         if (peso <= 0) {
-            throw new IllegalArgumentException("O peso deve ser positivo!");
+
+        if (peso <= 0) {
+            throw new IllegalArgumentException("O peso deve ser um valor positivo!");
         } else {
             this.peso = peso;
-        }       
-        
-        
-    }
+        }
 
+    }
+    
     public int getRpm() {
         return rpm;
-    }
-
-    public void setRpm(int rpm) {
-        
-         if (rpm <= 0) {
-            throw new IllegalArgumentException("O RPM deve ser positivo!");
-        } else {
-            this.rpm = rpm;
-        }  
-        
     }
 
     public int getVelocidade() {
         return velocidade;
     }
 
-    public void setVelocidade(int velocidade) {
-        
-        if (velocidade <= 0) {
-            throw new IllegalArgumentException("O RPM deve ser positivo!");
-        } else {
-            this.velocidade = velocidade;
-        }  
-        
-        
-    }
-
     public String getTipo() {
         return tipo;
-    }
-
-    public void setTipo(String tipo) {        
-        
-        
-        if (tipo.isEmpty()) {
-
-            throw new IllegalArgumentException("Preechimento obrigatório do tipo!");
-        } else {
-            this.tipo = tipo;
-
-        }     
-        
-        
     }
 
     public double getPreco() {
         return preco;
     }
 
+    // </editor-fold>
+    
+    
+    // <editor-fold defaultstate="collapsed" desc=" Setters ">
+
+    public void setRpm(int rpm) {
+
+        if (rpm <= 0) {
+            throw new IllegalArgumentException("O RPM deve ser positivo!");
+        } else {
+            this.rpm = rpm;
+        }
+
+    }
+
+    public void setVelocidade(int velocidade) {
+
+        if (velocidade <= 0) {
+            throw new IllegalArgumentException("O RPM deve ser positivo!");
+        } else {
+            this.velocidade = velocidade;
+        }
+
+    }
+
+    public void setTipo(String tipo) {
+
+        if (tipo.isEmpty()) {
+
+            throw new IllegalArgumentException("Preechimento obrigatório do tipo!");
+        } else {
+            this.tipo = tipo;
+
+        }
+
+    }
+
     public void setPreco(double preco) {
-        
-         if (preco <= 0) {
+
+        if (preco <= 0) {
             throw new IllegalArgumentException("O preço deve ser positivo!");
         } else {
             this.preco = preco;
         }
-    }    
+    }
+    
+    // </editor-fold>
     
     
-    // Métodos extras
+    // </editor-fold>
     
+    
+    // <editor-fold defaultstate="collapsed" desc=" Métodos extras ">
     public void Cadastrar(
             double peso,
             int rpm,
-            int velocidade,    
-            String tipo,    
+            int velocidade,
+            String tipo,
             double preco) {
         setPeso(peso);
         setRpm(rpm);
         setVelocidade(velocidade);
         setTipo(tipo);
-        setPreco(preco);        
-    };
+        setPreco(preco);
+    }
+
+    ;
     
     public void imprimir() {
 
@@ -168,8 +187,7 @@ public class Motor {
         System.out.println("Preço        : R$ " + String.format(Locale.GERMAN, "%,.2f", getPreco()));
 
     }
-    
-    
+
     public void entradaDados() {
         Scanner input = new Scanner(System.in);
         boolean continua = true;
@@ -180,44 +198,42 @@ public class Motor {
                 System.out.println("");
                 System.out.println("INSIRA OS DADOS SOLICITADOS:");
 
-                System.out.print("Peso         : ");
+                System.out.print("Peso (Kg)          : ");
                 setPeso(Double.parseDouble(input.nextLine()));
 
-                System.out.print("RPM          : ");
+                System.out.print("RPM                : ");
                 setRpm(Integer.parseInt(input.nextLine()));
 
-                System.out.print("Velocidade   : ");
+                System.out.print("Velocidade (Km/h)  : ");
                 setVelocidade(Integer.parseInt(input.nextLine()));
 
-                System.out.print("Tipo         : ");
-                setTipo(input.nextLine());                
+                System.out.print("Tipo (combustível) : ");
+                setTipo(input.nextLine());
 
-                System.out.print("Preço        : ");
+                System.out.print("Preço (BRL)        : ");
                 setPreco(Double.parseDouble(input.nextLine()));
 
                 continua = false;
 
             } catch (InputMismatchException inputMismatchException) {
-                
+
                 System.err.printf("\nException: %s\n", inputMismatchException);
                 System.out.println("Deve - se entrar com numeros reais. Tente de novo.\n");
-                
+
             } catch (NumberFormatException numberFormatException) {
-                
-                System.out.println("Dados inválidos, somente números são aceitos!");
+
+                System.err.println("Dados invalidados, digite novamente");
+                System.out.println("Somente números são aceitos!");
                 System.out.println(numberFormatException.getMessage());
 
-            } catch (Exception e) {
-                
-                System.out.println("Erro de operação, dados invalidados!");
-                System.out.println(e.getMessage());
+            } catch (Exception exception) {
+
+                System.err.println("Erro de operação, dados invalidados!");
+                System.out.println(exception.getMessage());
             }
 
         } while (continua);
     }
-    
-    
-    
-    
-    
+
+// </editor-fold>
 }
